1f4fd23be794142939088eebe513d5bab1ee4845
esianyo <esianicd@gmail.com>
2024-03-26 22:31:20 +0000

    ugraded to python

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,3 @@
+def update_topics(mongo_collection, name, topics):
+    """This function changes all topics of a school document based on the name"""
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
